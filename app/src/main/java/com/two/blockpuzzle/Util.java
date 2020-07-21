04301739dcb4b27f2e205b3fba82256ef22d8eea
flghthg <flghthg12345@gmail.com>
2020-07-21 12:00:38 +0900

    테트리스를 기반으로한 게임 프로그램!!

--- app/src/main/java/com/two/blockpuzzle/Util.java
+++ app/src/main/java/com/two/blockpuzzle/Util.java
@@ -2,7 +2,7 @@ package com.two.blockpuzzle;
 
 public interface Util {
 
-    String IP =  "192.168.123.104";
+    String IP =  "192.168.xxx.xxx";
 //            "
     String SERVER_IP = "http://"+IP+":9090/TetrisProject/Tetrislogin.jsp";
 
